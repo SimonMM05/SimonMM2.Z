6119522663a76167f0776183929b420fa6e28a7e
Simon Myrvig <simo008s@mail.hansenberg.dk>
2023-08-31 12:51:20 +0200

    Opdateret version af bog

--- Program.cs
+++ Program.cs
@@ -1,26 +1,42 @@
 ﻿List<Page> pages = new List<Page>();
-Page page1 = new Page("Min første side");
-Page page2 = new Page("Min side 2");
-Page page3 = new Page("Min side 3");
+Page PageVariabler = new Page("Min første side");
+Page PageDatatyper = new Page("Min side 2");
+Page PageArrays = new Page("Min side 3");
+Page PageLister = new Page("Min side 3");
+Page PageLoops = new Page("Min side 3");
+Page PageMethods = new Page("Min side 3");
+Page PageClasses = new Page("Min side 3");
 
-pages.Add(page1);
-pages.Add(page2);
-pages.Add(page3);
+pages.Add(PageVariabler);
+pages.Add(PageDatatyper);
+pages.Add(PageArrays);
+pages.Add(PageLister);
+pages.Add(PageLoops);
+pages.Add(PageMethods);
+pages.Add(PageClasses);
 
-Chapter Variabler = new Chapter("Kapitel 1");
+Chapter Variabler = new Chapter("Variabler");
 Variabler.Pages = pages;
-Chapter Datatyper = new Chapter("Kapitel 2", pages);
-
-Variabler.Read();
-Datatyper.Read();
+Chapter Datatyper = new Chapter("Datatyper", pages);
+Chapter Arrays = new Chapter("Arrays", pages);
+Chapter Lister = new Chapter("Lister", pages);
+Chapter Loops = new Chapter("Loops", pages);
+Chapter Methods = new Chapter("Methods", pages);
+Chapter Classes = new Chapter("Classes", pages);
 
 List<Chapter> Chapters = new List<Chapter>();
 Chapters.Add(Variabler);
 Chapters.Add(Datatyper);
+Chapters.Add(Arrays);
+Chapters.Add(Lister);
+Chapters.Add(Loops);
+Chapters.Add(Methods);
+Chapters.Add(Classes);
+
 
 Book mybook = new Book("Programmerings noter", Chapters);
 
 //pause
 Console.ReadLine();
 
-Book.Start();
+mybook.Start();
