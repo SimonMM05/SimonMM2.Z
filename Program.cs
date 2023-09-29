eb1e9649025515b3c673daa4aa048b4513c477e9
Simon Myrvig <simo008s@mail.hansenberg.dk>
2023-09-29 08:41:09 +0200

    VI har skrevet hvad hver linje kode gør så det er nemmere at forstå

--- Program.cs
+++ Program.cs
@@ -1,39 +1,38 @@
-﻿using System.Collections.Generic;
-using System.IO;
+﻿using System.Collections.Generic; // Importerer navnerummet for List<T>.
+using System.IO; // Importerer navnerummet for fil- og mappeoperationer.
 
+string dataDirectory = "Book_data"; // Definerer en strengvariabel med navnet dataDirectory og initialiserer den med stien til mappen "Book_data".
 
-    string dataDirectory = "Book_data";
-    List<Chapter> chapters = ReadChaptersFromDirectory(dataDirectory);
+List<Chapter> chapters = ReadChaptersFromDirectory(dataDirectory); // Opretter en liste af Chapter-objekter ved at kalde ReadChaptersFromDirectory-funktionen med dataDirectory som argument.
 
-    Book mybook = new Book("Programmerings noter", chapters);
-    mybook.Start();
+Book mybook = new Book("Programmerings noter", chapters); // Opretter en instans af Book-klassen med titlen "Programmerings noter" og listen over kapitler.
 
+mybook.Start(); // Kalder metoden Start på mybook-objektet.
 
-
-List<Chapter> ReadChaptersFromDirectory(string directoryPath)
+List<Chapter> ReadChaptersFromDirectory(string directoryPath) // Definerer en funktion med navnet ReadChaptersFromDirectory, der tager en sti til en mappe som argument og returnerer en liste af Chapter-objekter.
 {
-    List<Chapter> chapters = new List<Chapter>();
+    List<Chapter> chapters = new List<Chapter>(); // Opretter en tom liste til at gemme Chapter-objekter.
 
-    if (Directory.Exists(directoryPath))
+    if (Directory.Exists(directoryPath)) // Tjekker om den angivne directoryPath eksisterer.
     {
-        string[] chapterDirectories = Directory.GetDirectories(directoryPath);
+        string[] chapterDirectories = Directory.GetDirectories(directoryPath); // Får en liste af stier til undermapper inden for directoryPath.
 
-        foreach (string chapterDir in chapterDirectories)
+        foreach (string chapterDir in chapterDirectories) // Gennemløber hver undermappe.
         {
-            string chapterName = Path.GetFileName(chapterDir);
-            Chapter chapter = new Chapter(chapterName);
+            string chapterName = Path.GetFileName(chapterDir); // Udtrækker navnet på undermappen (kapitelnavnet).
+            Chapter chapter = new Chapter(chapterName); // Opretter et nyt Chapter-objekt med det udtrukne kapitelnavn.
 
-            string[] pageFiles = Directory.GetFiles(chapterDir, "*.txt");
+            string[] pageFiles = Directory.GetFiles(chapterDir, "*.txt"); // Får en liste af filstier med filtypen ".txt" inden for den aktuelle kapitelmappe.
 
-            foreach (string pageFile in pageFiles)
+            foreach (string pageFile in pageFiles) // Gennemløber hver sidefil.
             {
-                string pageContent = File.ReadAllText(pageFile);
-                chapter.Pages.Add(new Page(pageContent));
+                string pageContent = File.ReadAllText(pageFile); // Læser indholdet af sidefilen.
+                chapter.Pages.Add(new Page(pageContent)); // Opretter et nyt Page-objekt med sideindholdet og tilføjer det til kapitlets Pages-liste.
             }
 
-            chapters.Add(chapter);
+            chapters.Add(chapter); // Tilføjer kapitlet (med dets sider) til listen over kapitler.
         }
     }
 
-    return chapters;
+    return chapters; // Returnerer listen over kapitler.
 }
